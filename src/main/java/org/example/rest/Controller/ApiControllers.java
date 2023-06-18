3eeb5b7eef29f0a22c5ef76947dad7e249354918
Nicolas <nicolas-marquette@hotmail.fr>
2023-06-18 12:03:08 +0200

    Registration with username verification

--- src/main/java/org/example/rest/Controller/ApiControllers.java
+++ src/main/java/org/example/rest/Controller/ApiControllers.java
@@ -1,15 +1,12 @@
 package org.example.rest.Controller;
 
 import classes.User;
-import database.DatabaseConnector;
+import database.DatabaseConnection;
 import org.apache.logging.log4j.util.Strings;
 import org.jooq.*;
 import org.jooq.Record;
-import org.jooq.impl.DSL;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.jooq.impl.DSL.field;
@@ -28,20 +25,6 @@ public class ApiControllers {
 
         return "home";
     }
-//    @GetMapping("/register")
-//    public String registration() throws SQLException {
-//        DatabaseConnector db = new DatabaseConnector("nicolas","Ficellejulien66!");
-//        db.setRequest("INSERT INTO `users` (`id`, `username`, `password`, `mail`) VALUES (NULL, 'a', 'b', 'c');");
-//        db.connect();
-//        ResultSet result = db.execute();
-//        while(result.next()){
-//            System.out.println(result.getString(1)+" "+result.getString(2));
-//        }
-//        db.close();
-//
-//
-//        return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-//    }
 
     @PutMapping("/register")
     public ResponseEntity<String> register( @RequestBody User user) throws SQLException {
@@ -53,45 +36,36 @@ public class ApiControllers {
                 if (handleRegistration(user)) {
                     return ResponseEntity.ok("User successfully registered !");
                 }
+                return ResponseEntity.ok("An error was occured during registration !");
             }
             return ResponseEntity.ok("Username is already took !");
         }
-        return ResponseEntity.ok("An error was occured during registration !");
+        return ResponseEntity.ok("Fields empty !");
     }
 
     private boolean handleRegistration(User user) throws SQLException {
-        DatabaseConnector db = new DatabaseConnector();
-        DSLContext context = DSL.using(db.getConnection(), SQLDialect.MYSQL);
-        InsertValuesStep4<Record,Integer,String,String,String> insert = context
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        InsertValuesStep4<Record,Integer,String,String,String> insert = databaseConnection.getContext()
                 .insertInto(table("users"))
                 .columns(field("id",Integer.class), field("username",String.class), field("password",String.class), field("mail",String.class))
                 .values(1, user.getUsername(), user.getPassword(), user.getMail());
-
-        db.connect();
-        db.setRequest(insert.getSQL());
-        int numberRowsInjected = db.executeRegister(user);
-        db.close();
+        int numberRowsInjected = insert.execute();
 
         return numberRowsInjected == 1;
     }
 
     private boolean isUsernameAlreadyTook(User user) throws SQLException {
-        DatabaseConnector db = new DatabaseConnector();
 
-        DSLContext context = DSL.using(SQLDialect.MYSQL);
-        Table<Record> usersTable = table("users");
-        Field<String> usernameField = field("username", String.class);
+            DatabaseConnection databaseConnection = new DatabaseConnection();
+            SelectQuery<Record> query = databaseConnection.getContext().selectQuery();
+            Table<Record> usersTable = table("users");
+            Field<String> usernameField = field("username", String.class);
+            query.addSelect(usersTable.fields());
+            query.addFrom(usersTable);
+            query.addConditions(usernameField.eq(user.getUsername()));
+            // Execute the query
+            Result<Record> result = query.fetch();
 
-        SelectQuery<Record> selectQuery = context.selectQuery();
-        selectQuery.addFrom(usersTable);
-        selectQuery.addConditions(usernameField.eq(user.getUsername()));
-
-        selectQuery.execute();
-        db.connect();
-        db.setRequest(selectQuery.getSQL());
-        ResultSet result = db.verifyUsername(user);
-        db.close();
-
-        return result.wasNull();
+        return !result.isEmpty();
     }
 }
